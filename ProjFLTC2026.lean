bda3b0626141b589231edce8cc450fe297f552fd
Your Name <you@example.com>
2026-01-14 20:26:28 +0000

    ainda sem provas funcionais mas com as definições de cenas em comentários

--- ProjFLTC2026.lean
+++ ProjFLTC2026.lean
@@ -36,9 +36,36 @@ by
 -- signatures will be encoded with LEAN Languages
 -- Interpretation Structures will be encoded with LEAN Structures
 
+-- This is a how the key concepts are represented in Lean
+
+/-
+variable (Signature : FirstOrder.Language) -- signature Σ
+variable (Interpretion_Structure : Type)[Signature.Structure Interpretion_Structure]
+Interpretation Structure I
+variable (Assignment : α → Interpretion_Structure) Assignment ρ over I
+
+def Term.interpret (t : Signature.Term α) : (Interpretion_Structure):= Interpretation of terms with Iρ
+t.realize Assignment
+
+satistfaction
+def Formula.sat (φ : Signature.Formula α) :=
+    φ.Realize Assignment
+def BoundedFormula.sat (φ : Signature.BoundedFormula α n) :=
+    φ.Realize Assignment
+def Sentence.sat (φ : Signature.Sentence) :=
+    φ.Realize Interpretion_Structure
+
+
+entailment i.e. every model of a theory Θ satisfies φ
+def Formula.entail (φ : Signature.Formula α) :=
+    Thery.Model Interpretion_Structure → φ.Realize Assignment
+def Bounded.entail (φ : Signature.BoundedFormula α n) :=
+    Theory.Model Interpretion_Structure →  φ.Realize Assignment
+def Sentence.entail (φ : Signature.Sentence) :=
+    Theory.Model Interpretion_Structure → φ.Realize Interpretion_Structure
 
 open FirstOrder
-
+-/
 theorem and_def (S : FirstOrder.Language)
                 (M : Type) [S.Structure M]
                 (ρ : α → M)
@@ -59,11 +86,9 @@ inductive SRel : ℕ → Type
 
 def SSig : FirstOrder.Language := ⟨SFunc, SRel⟩ -- Simple signature with 0, S() and ≃
 
-abbrev eq : SSig.Relations 2 := .eq
+--abbrev eq : SSig.Relations 2 := .eq
 
-
-
-
+-- Defining the notation
 notation:50 t " ≅  " s =>
   FirstOrder.Language.BoundedFormula.rel SRel.eq ![t, s]
 
@@ -73,6 +98,7 @@ notation:50  "S("t")" =>
 notation:50 "zero" =>
   FirstOrder.Language.Term.func SFunc.zero ![]
 
+--Auxiliary recursive function to represent applying S() k times
 def succPow (n : ℕ) (t : SSig.Term α) :(SSig.Term α) :=
     match n with
     |0 => t
@@ -84,19 +110,27 @@ notation:50  "S^["k"]("t")" =>
 def S1 : SSig.Sentence := -- First Axiom
     ∀' (S(&(0)) ≅ zero).not
 
-def S2 : SSig.Sentence :=
+def S2 : SSig.Sentence := -- Second Axiom
     ∀' ∀' ((S(&(0)) ≅ S(&(1))).imp (&(0) ≅ &(1)))
 
-def S3 : SSig.Sentence :=
+def S3 : SSig.Sentence := -- Third Axiom
     ∀' ∃' ((&(1) ≅ zero).not.imp (S(&(0)) ≅ &(1)))
 
-def S4 (k : ℕ) : SSig.Sentence :=
+def S4 (k : ℕ) : SSig.Sentence := -- Fourth Axiom, actually ℵ₀ sentences
     ∀' ((S^[k](&(0))) ≅ &(0)).not
 
 
 def STheory : SSig.Theory :=
-    {S1, S2, S3} ∪ {S4 k | k : ℕ}
+    {S1, S2, S3} ∪ {S4 (k + 1) | k : ℕ}
+-- S4 actually represents one sentence for every natural number
+-- We use k+1 to skip the trivial case x=y → x=y
+
+def double_cycle : SSig.Sentence :=
+    ∀' ∀' ((S^[2](&(0)) ≅ S^[2](&(1))).imp (&(0) ≅ &(1)))
 
 theorem doubleS2 (M : Type)[SSig.Structure M]
                  (ρ : α → M):
-                 STheory.Model M → ( ∀' ∀' ((S^[2](&(0)) ≅ S^[2](&(1))).imp (&(0) ≅ &(1)))).Realize M ρ:=
+                 STheory.Model M → double_cycle.Realize M :=
+by
+    simp
+    intro h
